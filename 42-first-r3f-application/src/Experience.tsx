35efc198c6d778158bd57905ef3acc099009566b
JP <juan.bothma@gmail.com>
2022-10-29 11:27:13 +0200

    Animate Chapter + Fix Dependencies

--- 42-first-r3f-application/src/Experience.tsx
+++ 42-first-r3f-application/src/Experience.tsx
@@ -1,15 +1,27 @@
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import * as THREE from 'three';
+
 export default function Experience() {
+
+    const cubeRef = useRef<THREE.Mesh>(null!);
+
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta;
+    })
 
     return (
         <>
-            <mesh position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
+            <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
                 <boxGeometry />
                 <meshBasicMaterial color="mediumpurple" />
             </mesh>
+
             <mesh position-x={-2}>
                 <sphereGeometry />
                 <meshBasicMaterial color="orange" />
             </mesh>
+
             <mesh rotation-x={-Math.PI * 0.4} position-y={-1} scale={10}>
                 <planeGeometry />
                 <meshBasicMaterial color="greenyellow" />
