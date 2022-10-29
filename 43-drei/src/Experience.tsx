7c93acea3aaf294ad432f94100de51169f3b0436
JP <juan.bothma@gmail.com>
2022-10-29 13:35:12 +0200

    Transform controls chapter

--- 43-drei/src/Experience.tsx
+++ 43-drei/src/Experience.tsx
@@ -1,18 +1,24 @@
 import { OrbitControls } from '@react-three/drei'
+import { TransformControls } from '@react-three/drei';
+import { useRef } from 'react';
+import * as THREE from 'three';
 
 export default function Experience() {
+    const cube = useRef<THREE.Mesh>(null!);
+
     return (
         <>
-            <OrbitControls />
+            <OrbitControls makeDefault/>
 
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
             <group>
-                <mesh position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
+                <mesh position-x={2} scale={1.5} ref={cube}>
                     <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
+                <TransformControls object={cube}/>
 
                 <mesh position-x={-2}>
                     <sphereGeometry />
