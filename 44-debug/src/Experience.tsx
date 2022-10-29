3f2565f3b8ad907d3eb6735401e71eb9f7ce0f61
JP <juan.bothma@gmail.com>
2022-10-29 15:16:05 +0200

    leva chapter

---
+++ 44-debug/src/Experience.tsx
@@ -0,0 +1,53 @@
+import { OrbitControls } from '@react-three/drei'
+import { useControls, button } from 'leva';
+export default function Experience() {
+
+    const { position, color, visible } = useControls('Sphere', {
+        position: {
+            value: { x: -2, y: 0 },
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: '#FF0000',
+        visible: true,
+        clickMe: button(() => console.log('OK')),
+        choice: {
+            options: ['a', 'b', 'c']
+        }
+    });
+
+    const { scale } = useControls('Cube', {
+        scale: {
+            value: 1.5,
+            step: 0.01,
+            min: 0,
+            max: 5
+        }
+    });
+
+    return (
+        <>
+            <OrbitControls makeDefault />
+
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            <ambientLight intensity={0.5} />
+
+            <group>
+                <mesh position-x={2} scale={scale}>
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+
+                <mesh position={[position.x, position.y, 0]} visible={visible}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color={color} />
+                </mesh>
+            </group>
+
+            <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={20}>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+        </>
+    )
+}
---
+++ 44-debug/src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience';
+import React from 'react';
+import { Leva } from 'leva';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Leva collapsed />
+    <Canvas>
+      <Experience />
+    </Canvas>
+  </React.StrictMode>
+);
