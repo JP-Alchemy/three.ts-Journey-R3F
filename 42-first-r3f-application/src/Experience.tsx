e9bdba087848f1369c0d16e8837522bfcae4e466
JP <juan.bothma@gmail.com>
2022-10-29 11:30:23 +0200

    Group Chapter

--- 42-first-r3f-application/src/Experience.tsx
+++ 42-first-r3f-application/src/Experience.tsx
@@ -5,24 +5,28 @@ import * as THREE from 'three';
 export default function Experience() {
 
     const cubeRef = useRef<THREE.Mesh>(null!);
+    const groupRef = useRef<THREE.Group>(null!);
 
     useFrame((state, delta) => {
         cubeRef.current.rotation.y += delta;
+        groupRef.current.rotation.y += delta;
     })
 
     return (
         <>
-            <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
-                <boxGeometry />
-                <meshBasicMaterial color="mediumpurple" />
-            </mesh>
+            <group ref={groupRef}>
+                <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
+                    <boxGeometry />
+                    <meshBasicMaterial color="mediumpurple" />
+                </mesh>
 
-            <mesh position-x={-2}>
-                <sphereGeometry />
-                <meshBasicMaterial color="orange" />
-            </mesh>
+                <mesh position-x={-2}>
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange" />
+                </mesh>
+            </group>
 
-            <mesh rotation-x={-Math.PI * 0.4} position-y={-1} scale={10}>
+            <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
                 <planeGeometry />
                 <meshBasicMaterial color="greenyellow" />
             </mesh>
