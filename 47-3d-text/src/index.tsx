00b622ee95623df3916bc00591dad49a99e564bf
JP <juan.bothma@gmail.com>
2022-10-29 23:50:04 +0200

    update starter

--- 47-3d-text/src/index.tsx
+++ 47-3d-text/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import App from './App';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <App />
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [4, - 2, 6]
+    }}
+  >
+    <Experience />
+  </Canvas>
 );
